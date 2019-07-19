53d16e63d0134c9f95abff89ac536070e34ede81
MirkoPieri <mirko97.pieri@gmail.com>
2019-07-19 15:47:41 +0200

    prima parte funzioni JQuery Javascript.

--- script.js
+++ script.js
@@ -4,7 +4,29 @@
 
 $( document ).ready(function() {
 
+$(".next").click(nextimg)
 
+  function nextimg() {
+    var imgcount = $(".image img.active");
+    imgcount.removeClass("active");
+    if (imgcount.hasClass("last")) {
+      $(".first").addClass("active");
+    } else {
+      $(imgcount).next("img").addClass("active");
+    }
+  }
+
+  $(".prev").click(previmg)
+
+    function previmg() {
+      var imgcount = $(".image img.active");
+      imgcount.removeClass("active");
+      if (imgcount.hasClass("first")) {
+        $(".last").addClass("active");
+      } else {
+        $(imgcount).prev("img").addClass("active");
+      }
+    }
 
 
 
